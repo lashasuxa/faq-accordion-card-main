fa8c851c21824c9336881eb56c6329a7976c2341
lashasuxa <lashasukhashvili1995@gmail.com>
2023-03-26 01:35:31 -0400

    edit

--- script.js
+++ script.js
@@ -1,57 +1,23 @@
-// var questions = document.querySelectorAll("#question");
-// var span = document.querySelectorAll("span");
+var containers = document.querySelectorAll("#container");
+var panel = document.getElementsByTagName("span");
+var arrow_img = document.querySelectorAll("#arrow_img");
 
-// for (question of questions) {
-//   question.addEventListener("click", (event) => {
-//     console.log(event.target);
-//   });
-// }
-
-// implement toggle
-// implement parentNode of sibling
-
-// // foreach (container of containers) {
-// //   console.log(container);
-// // }
-
-// containers.forEach((container) => {
-//   container.addEventListener("click", console.log("hi"));
-// });
-
-// var containerOne = document.getElementById("containerOne");
-// var panel = document.getElementById("panel");
-// var arrow_img = document.getElementById("arrow_img");
-
-// containerOne.addEventListener("click", () => {
-//   panel.classList.toggle("newStyle");
-//   arrow_img.classList.toggle("rotateArrow");
-// });
-
-// var containers = document.querySelectorAll("#container");
-// var panel = document.getElementsByTagName("span");
-// console.log(panel);
-// var arrow_img = document.getElementById("arrow_img");
-
-// for (container of containers) {
-//   container.addEventListener("click", (event) => {
-//     console.log(event.target);
-//     panel.classList.toggle("newStyle");
-//     arrow_img.classList.toggle("rotateArrow");
-//   });
-// }
-
-// var containerOne = document.getElementById("containerOne");
-// console.log(containerOne.childNodes[3]);
-// containerOne.childNodes[3].classList.toggle("newStyle");
-
-var question = document.getElementById("question");
-console.log(question.parentNode.childNodes[3]);
-
-var paragraphs = document.getElementsByTagName("p");
-
-for (paragraph of paragraphs) {
-  paragraph.addEventListener("click", (event) => {
-    event.target.parentNode.childNodes[4].toggle("newStyle");
-    // console.log(event.target.parentNode.childNodes[4]);
+for (let i = 0; i < containers.length; i++) {
+  containers[i].addEventListener("click", function () {
+    panel[i].classList.toggle("newStyle");
+    arrow_img[i].classList.toggle("rotateArrow");
   });
 }
+
+// option with for of
+
+// const containers = document.querySelectorAll("#container");
+// const panels = document.getElementsByTagName("span");
+// const arrows = document.querySelectorAll("#arrow_img");
+
+// for (const [index, container] of containers.entries()) {
+//   container.addEventListener("click", () => {
+//     panels[index].classList.toggle("newStyle");
+//     arrows[index].classList.toggle("rotateArrow");
+//   });
+// }
